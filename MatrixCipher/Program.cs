34032bf9e5cc88900025499d6c3b8220a0a6c94a
MkSavin@mail.ru <mksavin@mail.ru>
2020-04-29 20:19:21 +0300

    Decryption realization

--- MatrixCipher/Program.cs
+++ MatrixCipher/Program.cs
@@ -20,22 +20,43 @@ namespace MatrixCipher {
                 inputString += input + '\n';
             } while (input != "");
 
+            Console.WriteLine("Input string [int]: ");
+            for (int i = 0; i < inputString.Length; i++) {
+                Console.Write((int)inputString[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
             var result = Matrixate(matrix, inputString);
 
-            Console.WriteLine("Result [int]:");
+            Console.WriteLine("Crypted result [int]:");
+            for (int i = 0; i < result.Length; i++) {
+                Console.Write((int)result[i] + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Crypted result:");
+            Console.WriteLine(result);
+
+            matrix = Inverse(matrix);
+
+            result = Matrixate(matrix, result);
+
+            Console.WriteLine("Decrypted crypted result [int]:");
             for (int i = 0; i < result.Length; i++) {
                 Console.Write((int)result[i] + " ");
             }
             Console.WriteLine();
-            Console.WriteLine("Result:");
+            Console.WriteLine("Decrypted crypted result:");
             Console.WriteLine(result);
+
             Console.ReadKey();
 
         }
 
-        static int[,] ReadInputArray() {
+        static float[,] ReadInputArray() {
 
-            int[,] result = new int[3, 3];
+            float[,] result = new float[3, 3];
 
             string input;
             int inputInt;
@@ -79,20 +100,20 @@ namespace MatrixCipher {
             return result;
         }
 
-        public static int[] MatrixMultiplyVector(int[,] matrix, int[] vector, int size = 3) {
+        public static float[] MatrixMultiplyVector(float[,] matrix, float[] vector, int size = 3) {
 
             if (matrix.GetLength(0) != size || matrix.GetLength(1) != size || vector.Length != size) {
                 return null;
             }
 
-            int[] result = new int[size];
+            float[] result = new float[size];
 
-            int c;
+            float c;
 
             for (int y = 0; y < size; y++) {
                 c = 0;
                 for (int x = 0; x < size; x++) {
-                    c += matrix[y, x] * vector[x];
+                    c += (float)Math.Round(matrix[y, x] * vector[x]);
                 }
                 result[y] = c;
             }
@@ -101,13 +122,13 @@ namespace MatrixCipher {
 
         }
 
-        public static string Matrixate(int[,] code, string inputString) {
+        public static string Matrixate(float[,] code, string inputString) {
 
             string result = "";
 
-            int[] vector = new int[3];
+            float[] vector = new float[3];
 
-            int[] resultVector = new int[3];
+            float[] resultVector = new float[3];
 
             for (int i = 0; i < inputString.Length; i++) {
                 vector[i % 3] = inputString[i];
@@ -131,6 +152,61 @@ namespace MatrixCipher {
             }
 
             return result;
+
+        }
+
+        public static float[,] Inverse(float[,] matrix) {
+
+            if (matrix.GetLength(0) != 3 || matrix.GetLength(1) != 3) {
+                return null;
+            }
+
+            var d3 = Det3(matrix);
+
+            float[,] result = new float[,] {
+                {
+                    Det2(new float[,] { { matrix[1, 1], matrix[1, 2] }, { matrix[2, 1], matrix[2, 2] } }) / d3,
+                    Det2(new float[,] { { matrix[0, 2], matrix[0, 1] }, { matrix[2, 2], matrix[2, 1] } }) / d3,
+                    Det2(new float[,] { { matrix[0, 1], matrix[0, 2] }, { matrix[1, 1], matrix[1, 2] } }) / d3,
+                },
+                {
+                    Det2(new float[,] { { matrix[1, 2], matrix[1, 0] }, { matrix[2, 2], matrix[2, 0] } }) / d3,
+                    Det2(new float[,] { { matrix[0, 0], matrix[0, 2] }, { matrix[2, 0], matrix[2, 2] } }) / d3,
+                    Det2(new float[,] { { matrix[0, 2], matrix[0, 0] }, { matrix[1, 2], matrix[1, 0] } }) / d3,
+                },
+                {
+                    Det2(new float[,] { { matrix[1, 0], matrix[1, 1] }, { matrix[2, 0], matrix[2, 1] } }) / d3,
+                    Det2(new float[,] { { matrix[0, 1], matrix[0, 0] }, { matrix[2, 1], matrix[2, 0] } }) / d3,
+                    Det2(new float[,] { { matrix[0, 0], matrix[0, 1] }, { matrix[1, 0], matrix[1, 1] } }) / d3,
+                }
+            };
+
+            return result;
+        }
+
+        public static float Det2(float[,] matrix) {
+
+            if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2) {
+                return 0;
+            }
+
+            return matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+
+        }
+
+        public static float Det3(float[,] matrix) {
+
+            if (matrix.GetLength(0) != 3 || matrix.GetLength(1) != 3) {
+                return 0;
+            }
+
+            return
+                matrix[0, 0] * matrix[1, 1] * matrix[2, 2] -
+                matrix[0, 0] * matrix[2, 1] * matrix[1, 2] -
+                matrix[1, 0] * matrix[0, 1] * matrix[2, 2] +
+                matrix[1, 0] * matrix[2, 1] * matrix[0, 2] +
+                matrix[2, 0] * matrix[0, 1] * matrix[1, 2] -
+                matrix[2, 0] * matrix[1, 1] * matrix[0, 2];
 
         }
 
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ How to use:
 1. Run program via Visual Studio *OR* via `.exe` file in `/bin/Debug` folder (need to compile);
 2. Enter the key (matrix 3x3) for cipher and press `Enter`;
 3. Enter input string to encrypt. `Enter` = new line. Empty line + `Enter` = next step;
-4. Get result.
+4. Get result (encrypted string with its decrypted version).
 
 # RU
 Matrix Cipher C#
@@ -14,4 +14,4 @@ Matrix Cipher C#
 1. Запустите программу через Visual Studio *ИЛИ* через файл `.exe` в папке `/bin/Debug` (нужно скомпилировать);
 2. Введите ключ (матрицу 3x3) для шифра и нажмите `Enter`;
 3. Введите строку ввода для шифрования. `Enter` = новая строка. Пустая строка + `Enter` = следующий шаг;
-4. Получите результат.
+4. Получите результат (шифрованную строку и ее дешифрованную версию).
